bf2da554c01fc46142993be18a7e1ad6e2e3e717
sun <xuhuading@foxmail.com>
2022-05-01 15:36:43 +0800

    fix: 以正确的方式引入veprogress

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+// 用户插件
+import veProgress from 'vue-ellipse-progress'
+
+createApp(App)
+    .use(store)
+    .use(veProgress, "vep")
+    .mount('#app')
